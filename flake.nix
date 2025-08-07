b25aad5a0a7053e48f45ee19217892abf17becc2
c0utin <rafaelcouto111@gmail.com>
2025-08-07 14:54:10 -0300

    [global] init

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      devShells = eachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
+          };
+
+          lib = pkgs.lib;
+
+          toolchain = fenix.packages.${system}.stable.withComponents [
+            "rustc"
+            "cargo"
+            "rustfmt"
+            "clippy"
+            "rust-src"
+          ];
+
+        in {
+          default = pkgs.mkShell {
+            name = "test-blocas-dev";
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              toolchain
+              solc
+              vyper
+              dprint
+              nodejs
+              foundry-bin
+            ] ++ lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.AppKit
+            ];
+
+            shellHook = ''
+              echo "Welcome to test-blocas dev shell 🦀"
+              export RUST_SRC_PATH="${toolchain}/lib/rustlib/src/rust/library"
+              export LD_LIBRARY_PATH="${lib.makeLibraryPath [ pkgs.libusb1 ]}"
+              export CFLAGS="-DJEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE"
+            '';
+          };
+        });
+    };
+}
